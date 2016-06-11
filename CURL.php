a60dbc9d3f18d0845ff178305dca14a7f8fde4d0
Noah R <CreationFox@users.noreply.github.com>
2016-06-11 12:48:25 -0500

    Update CURL.php

--- CURL.php
+++ CURL.php
@@ -1,1 +1,24 @@
+<?php
+// define variables and set to empty values
+$input = $auth = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $input = test_input($_POST["input"]);
+  $auth = test_input($_POST["auth"]);
+} else {
+echo "408";
+die();
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if ($auth == "000") {
+
+} else {
+  echo "401";
+?>
